36f7a2629ab92894fe09e9e633a198de91488c96
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-21 09:39:54 -0400

    update api calls

--- src/list.jsx
+++ src/list.jsx
@@ -1,26 +1,49 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+// import allPeople from "./assets/data.json";
 
 function List() {
-	const [data, setData] = useState(null);
+  const [allPeople, setAllPeople] = useState(null);
 
   useEffect(() => {
-    fetch("https://www.swapi.tech/api/people/1")
-      .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error(error));
+    console.log("fetching list of all people");
+    fetch("https://swapi.tech/api/people?page=1&limit=1000")
+      .then((response) => response.json())
+      .then((json) => setAllPeople(json))
+      .catch((error) => console.error(error));
   }, []);
+
+  const [people, setPeople] = useState([]);
+  let count = 0;
+
+  useEffect(() => {
+    function addPerson(person) {
+      setPeople([...people, person]);
+    }
+
+    console.log(`fetching ${count++} person...`);
+    if (allPeople) {
+      fetch(allPeople.results[0].url)
+        .then((response) => response.json())
+        .then((json) => addPerson(json))
+        .catch((error) => console.error(error));
+    }
+  }, [allPeople, count, people]);
 
   return (
     <div>
-     {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
-	  /*{data ? <p>{data.results[0].properties.name}</p> : 'No name'}*/
+      {/* {allPeople ? (
+        <pre>{JSON.stringify(allPeople, null, 2)}</pre>
+      ) : (
+        "Loading..."
+      )} */}
+      {/* {allPeople ? <p>{allPeople.results[0].properties.name}</p> : 'No name'} */}
+      {people.length > 0 ? (
+        <p>{people[0].result.properties.name}</p>
+      ) : (
+        "No name"
+      )}
     </div>
   );
 }
 
-export default List
-
-
-
-
-
+export default List;
