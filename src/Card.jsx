1e4c907177cfd03f978ecda60b139fa8563c1084
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-21 23:14:25 -0400

    style card background image

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,14 +4,10 @@ import "./styles/Card.css";
 function Card({ person }) {
   return (
     <div className="card">
-      <div className="imgPlaceholder">
-        <img
-          src={person.image}
-          alt={person.name}
-          width="100%"
-          height="100%"
-        ></img>
-      </div>
+      <div
+        className="imgPlaceholder"
+        style={{ backgroundImage: `url(${person.image})` }}
+      ></div>
       <p>{person.name}</p>
     </div>
   );
