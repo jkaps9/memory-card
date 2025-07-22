041d308ba1bf294e04c1cd7c0e0e9ab072b5c81e
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-21 23:58:24 -0400

    clicking card sets isClicked to true

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
 import "./styles/Card.css";
 
-function Card({ person }) {
+function Card({ person, setClicked }) {
   return (
-    <div className="card">
+    <div className="card" onClick={() => setClicked(person._id)}>
       <div
         className="imgPlaceholder"
         style={{ backgroundImage: `url(${person.image})` }}
       ></div>
       <p>{person.name}</p>
+      <p>{`${person.isClicked}`}</p>
     </div>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -4,23 +4,33 @@ import Card from "./Card";
 
 function List() {
   const [peopleList, setPeopleList] = useState(null);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   useEffect(() => {
     let rand = Math.floor(Math.random() * (952 / 12)) + 1;
 
     console.log("fetching list of all people");
     fetch(
-      `https://starwars-databank-server.vercel.app/api/v1/characters?page=${rand}&limit=12`
+      `https://starwars-databank-server.vercel.app/api/v1/characters?page=${rand}&limit=2`
     )
       .then((response) => response.json())
-      .then((json) => setPeopleList(json.data))
+      .then((json) =>
+        setPeopleList(json.data.map((obj) => ({ ...obj, isClicked: false })))
+      )
       .catch((error) => console.error(error));
   }, []);
+
+  function handleClick(personId) {
+    peopleList.filter((p) => p._id === personId)[0].isClicked = true;
+  }
 
   return (
     <div className="card-container">
       {peopleList
-        ? peopleList.map((person) => <Card person={person} key={person._id} />)
+        ? peopleList.map((person) => (
+            <Card person={person} setClicked={handleClick} key={person._id} />
+          ))
         : "Loading..."}
     </div>
   );
